dfcf670249d537545905ef31d4aa0174353c2d39
Justin Jeffries <justincyjeffries@gmail.com>
2022-11-28 16:47:01 -0500

    backend cleanup

--- app/models/helpme.rb
+++ app/models/helpme.rb
@@ -1,2 +1,7 @@
 class Helpme < ApplicationRecord
+    belongs_to :party_game_game
+    has_many :comments, dependent: :destroy
+
+    validates :party_game_game_id, {presence: true }
+    validates :question, {presence: true }
 end
--- db/migrate/20221128185700_create_party_game_games.rb
+++ db/migrate/20221128185700_create_party_game_games.rb
@@ -1,7 +1,7 @@
 class CreatePartyGameGames < ActiveRecord::Migration[6.1]
   def change
     create_table :party_game_games do |t|
-      t.belongs_to :party_game, null: false, foreign_key: true, array:true
+      t.belongs_to :party_game, null: false, foreign_key: true
       t.belongs_to :game, null: false, foreign_key: true
    
       t.string :status
